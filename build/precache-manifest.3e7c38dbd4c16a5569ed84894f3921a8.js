d76c626da9bd6ae1705d38005c9c6134d8d42caa
bruksbondd <bruksbond@gmail.com>
2020-11-16 15:24:28 +0200

    Add directory build

---
+++ build/precache-manifest.3e7c38dbd4c16a5569ed84894f3921a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9e8c488541aec337f21b61750d6f99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6279be58e6f892b90c13",
+    "url": "/static/css/2.4708e45d.chunk.css"
+  },
+  {
+    "revision": "54ba5f5860fa40caceea",
+    "url": "/static/css/main.f273c626.chunk.css"
+  },
+  {
+    "revision": "6279be58e6f892b90c13",
+    "url": "/static/js/2.fb1a3c91.chunk.js"
+  },
+  {
+    "revision": "b8509500cb0868084dfa4bbfe9a91cf1",
+    "url": "/static/js/2.fb1a3c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ba5f5860fa40caceea",
+    "url": "/static/js/main.cd23cad1.chunk.js"
+  },
+  {
+    "revision": "df966b0e753e3963cea4",
+    "url": "/static/js/runtime-main.80e47dc1.js"
+  },
+  {
+    "revision": "1acf3b9c57a83b3677f378d38ecf8f96",
+    "url": "/static/media/google.1acf3b9c.svg"
+  },
+  {
+    "revision": "05fcdd59eaa0d30a5aedabe28d19ed66",
+    "url": "/static/media/signOut.05fcdd59.svg"
+  }
+]);
